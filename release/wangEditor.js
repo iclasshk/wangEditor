fd080f5df5573bb1ffa2130d72a29371280aaf21
lenovo <tung@bull-b.com>
2018-12-07 17:46:13 +0800

    Revert "Update"
    This reverts commit 5dd0a03ce6354652eaaab999dca193e13b70d161.

--- release/wangEditor.js
+++ release/wangEditor.js
@@ -1417,7 +1417,7 @@ Link.prototype = {
                         var $link = $('#' + inputLinkId);
                         var $text = $('#' + inputTextId);
                         var link = $link.val();
-                        var text = $text.html();
+                        var text = $text.val();
                         _this._insertLink(text, link);
 
                         // 返回 true，表示该事件执行完之后，panel 要关闭。否则 panel 不会关闭
